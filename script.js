8cf742bed90980f6df0f5454fb72f42babba9bf7
Prashant Verma <prashant.verma@springrole.com>
2019-01-22 17:17:57 +0530

    test

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ var name = prompt("What is \'that\' your name?");
 var message = "Hello ";
 alert(message + name);
 
-
-var a = 100;
+asdasdas
+var a = 100; dasfasfdfasd
 var b = 150;
 var result = (a+b)
+asdasdasddsa
